6a0d90a707d9e28adf6bc471431fa5f74400ebec
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-05 23:37:16 +0530

    computer-list-model

--- src/app/computers/computer-list/computer-list.component.ts
+++ src/app/computers/computer-list/computer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ComputerService } from '../shared/computer.service'
 
 @Component({
   selector: 'app-computer-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComputerListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private computerService : ComputerService) { }
 
   ngOnInit() {
   }
--- src/app/computers/computers.component.ts
+++ src/app/computers/computers.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ComputerService } from './shared/computer.service'
 @Component({
   selector: 'app-computers',
   templateUrl: './computers.component.html',
-  styleUrls: ['./computers.component.css']
+  styleUrls: ['./computers.component.css'],
+  providers :[ComputerService]
 })
 export class ComputersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private computerService : ComputerService) { }
 
   ngOnInit() {
   }
