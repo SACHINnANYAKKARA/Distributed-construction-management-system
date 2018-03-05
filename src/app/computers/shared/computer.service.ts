07b9a75f91da674a05710ff2348904cfbc15f18b
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-03-05 23:04:54 +0530

    computer-list-model

--- src/app/computers/shared/computer.service.ts
+++ src/app/computers/shared/computer.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'
+import { Computer} from './computer.model';
 
 @Injectable()
 export class ComputerService {
+  computerList: AngularFireList<any>;
+  selectedComputer: Computer = new Computer();
+  constructor(private firebase :AngularFireDatabase ) { }
 
-  constructor() { }
+  getData(){
+    this.computerList = this.firebase.list('computers');
+    return this.computerList;
+  }
+
+  insertComputer(computer : Computer)
+  {
+    this.computerList.push({
+      name: computer.name,
+      version: computer.version,
+      type: computer.type,
+      price: computer.price
+    });
+  }
+ 
+  updateComputer(computer : Computer){
+    this.computerList.update(computer.$key,
+      {
+      name: computer.name,
+      version: computer.version,
+      type: computer.type,
+      price: computer.price
+      });
+  }
+
+  deleteComputer($key : string){
+    this.computerList.remove($key);
+  }
+
 
 }
