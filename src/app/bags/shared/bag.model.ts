76027ab49fd7b63832a2b1e509976cf97c2bf8fb
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2018-04-20 22:28:52 +0530

    patient

--- src/app/bags/shared/bag.model.ts
+++
@@ -1,7 +0,0 @@
-export class Bag {
-    $key : string;
-    name : string;
-    position : string;
-    office : string;
-    salary : number;
-}
--- src/app/computers/shared/computer.model.ts
+++
@@ -1,9 +0,0 @@
-export class Computer {
-  
-        $key : string;
-        name : string;
-        version : string;
-        type : string;
-        price : number;
-    
-}
